b53e1d3337cda5ea056642a377a3cd19223dd996
Timothy Cosgriff <timothy.cosgriff@gmail.com>
2023-04-17 22:43:44 -0400

    Various servonet and nas changes

--- provision/terraform/gpc/servonet/node/main.tf
+++ provision/terraform/gpc/servonet/node/main.tf
@@ -8,31 +8,32 @@ terraform {
   required_version = ">= 0.12"
 }
 
-resource "null_resource" "cloud_init_config_file" {
-  connection {
-    type = "ssh"
-    user = "${var.pve_user}"
-    password = "${var.pve_password}"
-    host = "${var.pve_host}"
-  }
+# resource "null_resource" "cloud_init_config_file" {
+#   connection {
+#     type = "ssh"
+#     user = "${var.pve_user}"
+#     password = "${var.pve_password}"
+#     host = "${var.pve_host}"
+#   }
 
-  provisioner "file" {
-    content = "${templatefile("${path.module}/files/user-data.tpl", {hostname = "${var.name}", username = "${var.ciuser}", passwd = "${var.cipassword_hashed}"})}"
-    destination = "/var/lib/vz/snippets/${var.name}.yml"
-  }
-}
+#   provisioner "file" {
+#     content = "${templatefile("${path.module}/files/user-data.tpl", {hostname = "${var.name}", username = "${var.ciuser}", passwd = "${var.cipassword_hashed}"})}"
+#     destination = "/var/lib/vz/snippets/${var.name}.yml"
+#   }
+# }
 
 
 resource "proxmox_vm_qemu" "servonet-node" {
-  depends_on = [
-    null_resource.cloud_init_config_file
-  ]
+#   depends_on = [
+#     null_resource.cloud_init_config_file
+#   ]
 
   name = "${var.name}"
   desc = "servonet node"
   target_node = var.target_node
 
   clone = var.template_name
+  full_clone = false
 
   cores = var.cores
   sockets = 1
@@ -44,22 +45,27 @@ resource "proxmox_vm_qemu" "servonet-node" {
     size = var.storage_size
   }
   // secondary disk for block storage
-  disk {
-    type = "scsi"
-    storage = "vmstoragelimited"
-    size = "100G"
-  }
+#   disk {
+#     type = "scsi"
+#     storage = "vmstoragelimited"
+#     size = "100G"
+#   }
 
   network {
     model = "virtio"
     bridge = var.bridge
-    macaddr =  var.macaddr
+    # macaddr =  var.macaddr
   }
 
-  cicustom = "user=local:snippets/${var.name}.yml"
-  cloudinit_cdrom_storage = "iso"
+#   cicustom = "user=local:snippets/${var.name}.yml"
+#   cloudinit_cdrom_storage = "local"
 
   os_type = "cloud-init"
   ipconfig0 = "ip=${var.ip}/16,gw=${var.gateway}"
-
+  ssh_user        = "tom"
+  ssh_private_key = <<EOF
+-----BEGIN RSA PRIVATE KEY-----
+private ssh key tom
+-----END RSA PRIVATE KEY-----
+EOF
 }
---
+++ provision/terraform/gpc/servonet/variables.tf
@@ -0,0 +1,4 @@
+# variable "pm_api_url" {
+#   description = "API URL for Proxmox"
+#   type = string
+# }
