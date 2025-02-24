8266e6f6ac04e4ba0244e17f861e4b65be46f462
Timothy Cosgriff <lambchop4prez@users.noreply.github.com>
2025-02-23 22:51:15 -0500

    feat: Add devShells

--- flake.nix
+++ flake.nix
@@ -7,11 +7,14 @@
 
   outputs = inputs@{ self, nixpkgs }:
   let
+    darwinSystems = ["aarch64-darwin" "x86_64-darwin"];
+    linuxSystems = ["aarch64-linux" "x86_64-linux"];
+    allSystems = darwinSystems ++ linuxSystems;
     hosts = {
-      default = {
-        system = "x86_64-linux";
-        runtime = "qemu-vm";
-      };
+      # default = {
+      #   system = "x86_64-linux";
+      #   runtime = "qemu-vm";
+      # };
       ca = {
         system = "x86_64-linux";
         runtime = "proxmox-lxc";
@@ -20,6 +23,26 @@
     hostnames = nixpkgs.lib.attrNames hosts;
   in
   {
+    devShells = nixpkgs.lib.genAttrs allSystems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            opentofu
+            zsh
+          ];
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      }
+    );
     nixosConfigurations = nixpkgs.lib.genAttrs hostnames (
       host: nixpkgs.lib.nixosSystem {
         # inherit host;
--- modules/qemu-vm/default.nix
+++ modules/qemu-vm/default.nix
@@ -1,7 +1,25 @@
-{ config, modulesPath, pkgs, lib, ... }:
+{ modulesPath, ... }:
 {
   imports = [
-    (modulesPath + "/virtualisation/qemu-vm.nix")
-    (modulesPath + "/virtualisation/qemu-guest-agent.nix")
+    "${modulesPath}/profiles/qemu-guest.nix"
+    "${modulesPath}/virtualisation/qemu-guest-agent.nix"
   ];
+  config.services.qemuGuest.enable = true;
+  # services.openssh.enable = true;
+
+  # boot.initrd.availableKernelModules =
+  #   [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+  # boot.initrd.kernelModules = [ ];
+  # boot.kernelModules = [ "kvm-intel" ];
+  # boot.extraModulePackages = [ ];
+
+  # boot.kernelParams = [
+  #   "console=tty1"
+  #   "console=ttyS0,115200"
+  # ];
+
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
 }
