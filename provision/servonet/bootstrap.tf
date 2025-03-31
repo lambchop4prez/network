5e3bdaccf7e90f7d5ae94dc42f23d1b6c88b6957
Timothy Cosgriff <lambchop4prez@users.noreply.github.com>
2025-03-31 00:31:47 -0400

    fix: Syntax fixes

--- provision/servonet/bootstrap.tf
+++ provision/servonet/bootstrap.tf
@@ -2,10 +2,9 @@ resource "time_sleep" "wait_10_seconds" {
   create_duration = "10s"
 }
 module "bootstrap" {
-  source                          = "./modules/bootstrap"
-  kubernetes_client_configuration = module.cluster.kubernetes_client_configuration
-  cluster_domain                  = var.cluster_name
-  deploy_key                      = tls_private_key.flux.public_key_openssh
-  deploy_private_key              = tls_private_key.flux.private_key_pem
-  depends_on                      = [time_sleep.wait_10_seconds]
+  source             = "./modules/bootstrap"
+  cluster_domain     = var.cluster_name
+  deploy_key         = tls_private_key.flux.public_key_openssh
+  deploy_private_key = tls_private_key.flux.private_key_pem
+  depends_on         = [time_sleep.wait_10_seconds]
 }
