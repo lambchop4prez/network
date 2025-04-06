de6759f573b1817f142b7096a4b731e8b1a52724
Timothy Cosgriff <lambchop4prez@users.noreply.github.com>
2025-04-06 14:00:52 -0400

    feat: Bootstrap using flux operator

--- provision/servonet/bootstrap.tf
+++ provision/servonet/bootstrap.tf
@@ -3,8 +3,8 @@ resource "time_sleep" "wait_10_seconds" {
   depends_on      = [module.cluster]
 }
 module "bootstrap" {
-  source             = "./modules/bootstrap"
-  cluster_domain     = var.cluster_name
+  source = "./modules/bootstrap"
+  # cluster_domain     = var.cluster_name
   deploy_key         = tls_private_key.flux.public_key_openssh
   deploy_private_key = tls_private_key.flux.private_key_pem
   depends_on         = [time_sleep.wait_10_seconds]
